480e7ec4c684db04547ba8062d2deecd854de5e3
Rui Barbosa <rpedro10@hotmail.com>
2023-08-13 18:48:17 +0100

    Add curriculum formated as code

---
+++ curriculum.js
@@ -0,0 +1,177 @@
+
+/*** CURRICULUM VITAE **/
+
+
+/*
+* PERSONAL INFORMATION
+*/
+var PersonalInfo = {
+    name:        'Rui Barbosa',
+    location:    'Rua Chaimite - 4435 - Gondomar',
+    phone:       '963748225',
+    email:       'rpabarbosa@gmail.com',
+    description: 'Software Engineer',
+};
+
+
+/*
+* PROFESSIONAL EXPERIENCE
+*/
+var Experience = {
+    
+    2019-now: {
+        
+        position: 'Software Engineer',
+        employee: 'Sky Technology Hub',
+        responsability: [
+            'Develop and maintain a streaming service UI, business logic and the BFF layer (backend for frontend) on a high scale high performance environment',
+            'Teach, mentor and manage junior developers and summer internships',
+            'Interview and prospect for technical needs',
+        ],
+        products: ['Peacock', 'SkyShowtime', 'ShowMax'],
+    },
+
+    2018-2019: {
+
+        position: 'Senior Web Engineer/Team Lead',
+        employee: 'Hexis Technology Hub',
+        responsability: [
+            'Consulting and advice of technological and creative solutions for solving internal and clients problems',
+            'Develop and mantain a happy and agile team',
+            'Bridging with the client',
+        ],
+        clients: ['Farfetch'],
+    },
+
+    2017-2018: {
+
+        position: 'Senior Web Engineer',
+        employee: 'log OSCON',
+        responsability:	[
+            'Arquitect, development and maintenance of web solutions for big companies.',
+            'Consulting and advice of technological and creative solutions for solving internal and clients problems',
+            'Co-organizer of company conference(change.log) and contributor to company standards',
+        ],
+        clients: ['RTP', 'Sonae', 'NOS', 'Ageas', ...],
+
+    },
+
+    2015-2016: {
+
+        position: 'Full-Stack Developer',
+        employee: 'Brief Creatives',
+        responsability:	[
+            'Development and maintenance of static and dynamic website (using Wordpress), hybrid mobile aplications (using Ionic) and APIs (using Node)',
+            'Consulting and advice of technological and creative solutions for solving internal and clients problems',
+            'Maintaining and managing the company servers',
+        ],
+        clients: ['Foodways', 'Final Fantasy DW', 'Thrombocid', 'Emptmeyer', ...],
+
+    },
+
+    2012-2015: {
+
+        position:	'Full-Stack Developer',
+        employee:	'Visual-Kitchen',
+        responsability: [
+            'Development and maintenance of static and dynamic website (using Wordpress), graphic aplications (using Flash), web platforms and APIs (using Laravel)',
+            'Consulting and advice of technological and creative solutions for solving internal and clients problems',
+            'Maintaining and managing the company server',
+            'Mentoring, collaborate and monitoring of and with IT interns',
+        ],
+        clients: ['Walk&Talk', 'Governo dos Açores', 'São Paulo FC', ...]
+
+    },
+
+    2012-now: {
+
+        position:  'Full-Stack Developer',
+        employee:	'Freelancer',
+        responsability:	'Consulting and development of business solutions',
+
+    },
+
+};
+
+
+/*
+*  TECHNICAL AND PERSONAL SKILLS
+*/
+var TechnicalSkills = {
+
+    FrontEnd: {
+        style: 'CSS w/ preprocessors(SASS, PostCSS, etc.), BEM/BEVM, Atomic  Design',
+        libraries: {
+            state:      [ 'Redux' ],
+            animations: [ 'GSAP', 'Velocity.js' ],
+            other:      [ 'lodash' ],
+        },
+        frameworks: 'VanillaJS, ReactJS, AngularJS, LightningJS, Ionic',
+        superset: 'Typescript',
+    },
+
+    BackEnd: {
+        JavaScript: [ 'NodeJS with Express, GraphQL' ],
+        PHP:        [ 'Wordpress', 'Laravel' ],
+        other:      [ 'Golang' ],
+        databases:  [ 'MySQL', 'MongoDB' ],
+    },
+
+    UX/UI: [
+        'Continuous collaboration with designers and product owners to make pixel perfect interfaces, using Sketch, Zeplin, Illustrator and/or Figma',
+        'Prototyping UX animations using online tools, like CodePen.',
+    ],
+
+    Workflow: {
+        paradigms: 'Knowledge in object oriented and functional programing',
+        arquitectures: 'Knowledge in MV* arquitectures and Event-Driven',
+        virtual_environments: 'Vagrant, Docker',
+        version_control: 'git, normaly following gitflow guidelines',
+        deployment: 'Codeship, Jenkins',
+        package_manager: 'composer, npm, yarn, pnpm, lerna',
+        build_tools: 'Browserify, Babel, Webpack',
+        code_style: 'ESLint, Stylelint, PHPCS, EditorConfig',
+        testing: 'Jest, Cypress, k6'
+    },
+
+    Other: [
+        'Processing',
+        'PureData/M4L',
+    ],
+
+};
+
+var InterpersonalSkills = [
+    'Colaborative',
+    'Creative',
+    'Curious',
+    'Multidisciplinary',
+    'Resiliant',
+];
+
+/*
+* EDUCATION
+*/
+var Education = {
+
+    2009-2011: {
+        school: 'Instituto Superior de Engenharia do Porto',
+        course: 'Master, Computer Science – Graphics Systems and Multimedia',
+    },
+
+    2005-2009: {
+        school: 'Instituto Superior de Engenharia do Porto',
+        course: 'Degree, Informatics Engineering',
+    },
+
+};
+
+/*
+* PERSONAL INTERESTS
+*/
+var PersonalInterest = [
+    'Teaching/Speaking',
+    'Creative Coding',
+    'Gaming',
+    'Music Production',
+];
